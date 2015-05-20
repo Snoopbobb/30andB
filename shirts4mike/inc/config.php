25cd6c47b1b1932028d20a8d8c64b5d51a57be8c
Bob Tabor <bobtabor@q.com>
2015-05-20 13:04:58 -0700

    Delete config.php

--- shirts4mike/inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	define("BASE_URL", "/shirts4mike/");
-	define("ROOT_PATH" , $_SERVER["DOCUMENT_ROOT"] . "/shirts4mike/");
-
-	define("DB_HOST", "localhost");
-	define("DB_NAME", "shirts4mike");
-	define ("DB_USER", "root"); 
-	define ("DB_PASS", "Rose1974!");
